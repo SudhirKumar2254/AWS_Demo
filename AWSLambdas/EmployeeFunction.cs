6c9cc2f369755c0b91d0418272fe1fa9955b96ec
Sudhir Kumar <81869981+SudhirKumar2254@users.noreply.github.com>
2023-02-26 12:34:13 +0530

    .ignore

--- AWSLambdas/EmployeeFunction.cs
+++ AWSLambdas/EmployeeFunction.cs
@@ -70,6 +70,7 @@ public class EmployeeFunction
         var queryResponse = await _dynamoDbClient.QueryAsync(queryRequest);
 
         //If circuit is closed do work else put the message in DB
+
         if (queryResponse.Count > 0)
         {
             var response = await PostData(request, context);
